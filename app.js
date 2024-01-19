25027ab69e2490eec5f54dc8ddfe75e5ec58a77a
MichaelZolotov77 <michaelzol@yandex.ru>
2024-01-19 19:49:28 +0300

    Если ошибки в адресе нет. fetch()

--- app.js
+++ app.js
@@ -1,26 +1,12 @@
-function withPromose() {
-  return new Promise((resolve, reject) => {
-    reject("Hello from Promise");
-  });
-}
-
-console.log("Hi there, me first");
-
-setTimeout(() => console.log("When is my turn?"), 0);
-
-withPromose()
+fetch("https://jsonplaceholder.typicode.com/posts/1")
+  .then((response) => {
+    console.log(response);
+    if (response.ok) {
+      return response.json(); // преобразовывает json в объект JS
+    }
+    throw new Error("Failed to fetch");
+  })
   .then(console.log)
-  .catch((val) => console.log(val + "!!!"));
+  .catch(console.error); // Failed to fetch
 
-console.log("Am I list in the queue?");
-
-// Hi there, me first
-// app.js:15 Am I list in the queue?
-// Hello from Promose
-// app.js:9 When is my turn?
-
-/* 
-Event Loop имеет две области - task queue и microtask queue.
-Promise попадает в microtask и поэтому выполняется быстрее, чем
-обычный task queue, в который попадает setTimeout()
-*/
+// Если ошибки в адресе нет
