68fbfa248fae58c5c4dde75c439ca6aeb71f36cb
MichaelZolotov77 <michaelzol@yandex.ru>
2024-01-19 16:22:37 +0300

    Init

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("Hi there, me first");
+
+setTimeout(() => console.log("When is my turn?"), 0);
+
+console.log("Am I list in the queue?");
+
+// Hi there, me first
+// Am I list in the queue?
+// When is my turn?
