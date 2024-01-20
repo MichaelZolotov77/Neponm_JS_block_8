d882f33d2b7e70aafba084017cd74f9b0084972b
MichaelZolotov77 <michaelzol@yandex.ru>
2024-01-20 19:10:35 +0300

    Legacy способ получения данных через  XMLHttpRequest.

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
-// fetch по умолчанию работает как GET
-fetch("https://jsonplaceholder.typicode.com/posts", {
-  method: "POST",
-  body: JSON.stringify({
-    id: 201,
-    title: "My title",
-    body: "Text",
-    userId: 1,
-  }),
-  headers: {
-    "Content-Type": "application/json",
-  },
-})
-  .then((response) => {
-    console.log(response);
-    if (response.ok) {
-      return response.json(); // преобразовывает json в объект JS
-    }
-    throw new Error("Failed to fetch");
-  })
-  .then(console.log)
-  .catch(console.error);
+// Это legacy способ, но в старых проектах встечается
 
-// CRUD - create, read,   update,  delete
-// HTTP - POST,   GET,  PUT|PATCH, DELETE
+const url = "https://jsonplaceholder.typicode.com/posts/1";
+
+const request = new XMLHttpRequest();
+console.log(request);
+request.open("GET", url);
+request.responseType = "json"; // xml, text
+request.send(); // отправляем запрос
+// ловим ответ
+request.onload = function () {
+  const data = request.response;
+  console.log(data); // приходит уже объект, преобразованный из json
+};
