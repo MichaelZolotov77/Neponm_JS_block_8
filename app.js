6f3ac64eac56d915dd2b3324a2a1fa6ef13466d6
MichaelZolotov77 <michaelzol@yandex.ru>
2024-01-20 19:32:03 +0300

    Проверка на ошибки try catch

--- app.js
+++ app.js
@@ -26,9 +26,14 @@
 
 // если сделать return, async функция всегда возвращает promise
 async function getUsers() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const data = await response.json();
-  console.log(data);
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await response.json();
+    console.log(data);
+    throw new Error("Failed!");
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
 // const gerUser = async() => {}
